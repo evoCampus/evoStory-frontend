93fd2a2888290f6f763a2b759f1d3a00d48e9fc2
Gyulai Milán <gyulai.milan04@gmail.com>
2025-04-14 16:01:57 +0200

    Started routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ChapterPage from './pages/chapter/ChapterPage';
+import HomePage from './pages/chapter/ChapterPage';
+
+
 function App() {
-
-
-  return (<div className="bg-white">
-    Szöveg
-  </div>)
+  return (
+      <Router>
+          <Routes>
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/chapter" element={<ChapterPage />} />
+          </Routes>
+      </Router>
+  );
 }
+
 
 export default App
---
+++ src/pages/chapter/ChapterPage.tsx
@@ -0,0 +1,26 @@
+import '../index.css';
+import Button from '../../components/Button';
+
+function ChapterPage() {
+    return (
+        <div className="flex-auto items-center justify-center h-full w-screen">
+            <div className="flex-auto border border-gray-400 p-4 mx-auto w-4/5 gap-4">
+                <div className="flex items-center justify-center h-48 border border-gray-400">
+                    <span className="text-center">Ez a szöveg az 1. dobozban van.</span>
+                </div>
+                <div className="flex items-center justify-center h-48 border border-gray-400 mt-4">
+                    <span className="text-center">Ez a szöveg a 2. dobozban van.</span>
+                </div>
+                <div className="flex items-center justify-center h-48 border border-gray-400 mt-4">
+                    <span className="text-center">Ez a szöveg a 3. dobozban van.</span>
+                </div>
+                <div className='flex  items-center content-center place-content-evenly'>
+                <Button text="Gomb 1" className="mt-4"/>
+                <Button text="Gomb 2" className="mt-4 bg-green-500 hover:bg-green-600" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ChapterPage;
