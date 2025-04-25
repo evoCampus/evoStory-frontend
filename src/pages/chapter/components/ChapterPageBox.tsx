c4d4e044da7ba6db1a41b244b83af45f41aa1e4d
MilanGyulai <149778992+MilanGyulai@users.noreply.github.com>
2025-04-25 11:54:26 +0200

    Formatted and changed export const on ChapterPageBox.tsx

--- src/pages/chapter/components/ChapterPageBox.tsx
+++ src/pages/chapter/components/ChapterPageBox.tsx
@@ -5,12 +5,11 @@ interface Box {
     className?: string;
 }
 
-const ChapterPageBox: React.FC<Box> = ({text, className}) => {
-    return(
-    <div className={`flex items-center justify-center h-48 border border-gray-400 mt-4 ${className || ''}`}>
-    <span className="text-center">{text}</span>
-    </div>
+export const ChapterPageBox: React.FC<Box> = ({ text, className }) => {
+    return (
+        <div className={`flex items-center justify-center h-48 border border-gray-400 mt-4 ${className || ''}`}>
+            <span className="text-center">{text}</span>
+        </div>
     );
 }
 
-export default ChapterPageBox;
