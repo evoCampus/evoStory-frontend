1a32660f649ab438910d9cf55fe3368f1ec16714
MilanGyulai <149778992+MilanGyulai@users.noreply.github.com>
2025-04-26 13:17:21 +0200

    Updated export default function jsx for ChapterPageBox.tsx

--- src/pages/chapter/components/ChapterPageBox.tsx
+++ src/pages/chapter/components/ChapterPageBox.tsx
@@ -1,15 +1,14 @@
-import React from "react";
+import { JSX } from "react";
 
 interface Box {
     text: string;
     className?: string;
 }
 
-export const ChapterPageBox: React.FC<Box> = ({ text, className }) => {
+export default function ChapterPageBox({ text, className }: Box): JSX.Element {
     return (
         <div className={`flex items-center justify-center h-48 border border-gray-400 mt-4 ${className || ''}`}>
             <span className="text-center">{text}</span>
         </div>
     );
 }
-
