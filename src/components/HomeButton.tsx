ccc1cb0707f3955d65a9a049dcb930fd144596aa
MilanGyulai <149778992+MilanGyulai@users.noreply.github.com>
2025-04-28 19:08:35 +0200

    Updated to Link HomeButton.tsx

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,24 +1,16 @@
+import { Link } from "react-router-dom";
 import { JSX } from "react";
-import { useNavigate } from "react-router-dom";
 
-interface BackButtonProps {
+interface HomeButtonProps {
     className?: string;
 }
 
-export default function HomeButton({ className }: BackButtonProps): JSX.Element {
-    const navigate = useNavigate();
-    const handleNavigateToHome = () => {
-        navigate('/');
-    }
-    }
-
+export default function HomeButton({ className }: HomeButtonProps): JSX.Element {
     return (
         <div className={"mt-auto p-4 text-center"}>
-            <button
-                onClick={handleNavigateToHome}
-                className={`bg-gray-900 rounded-xl text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outlinetransition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500  ${className || ''}`}>
+            <Link to="/" className={`roundButton text-center ${className || ''}`}>
                 <span>Main</span>
-            </button>
+            </Link>
         </div>
     );
 }
