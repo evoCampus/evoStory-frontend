149ea0e946bd4bdad4fdb5b697c7a55fd9ef9293
MilanGyulai <149778992+MilanGyulai@users.noreply.github.com>
2025-04-28 19:07:08 +0200

    deleted unnecesarry lines HomeButton.tsx

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,5 +1,4 @@
 import { JSX } from "react";
-import { JSX } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface BackButtonProps {
@@ -8,22 +7,18 @@ interface BackButtonProps {
 
 export default function HomeButton({ className }: BackButtonProps): JSX.Element {
     const navigate = useNavigate();
-
     const handleNavigateToHome = () => {
         navigate('/');
     }
     }
 
     return (
-    return (
         <div className={"mt-auto p-4 text-center"}>
             <button
                 onClick={handleNavigateToHome}
-                className={`bg-gray-900 rounded-xl text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outlinetransition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500  ${className || ''}`}
-            >
+                className={`bg-gray-900 rounded-xl text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outlinetransition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500  ${className || ''}`}>
                 <span>Main</span>
             </button>
         </div>
-    );
     );
 }
