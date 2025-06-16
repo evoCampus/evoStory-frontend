61ac09a3031887516b878beb378429e4c70707fe
MilanGyulai <149778992+MilanGyulai@users.noreply.github.com>
2025-06-16 19:15:47 +0200

    Typo fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
           <React.StrictMode>
         <App />
-          </React.StrictmMode>
+          </React.StrictMode>
 );
