c2cedbb5de0779ddf00fff2eee7709c8a6c65521
MilanGyulai <149778992+MilanGyulai@users.noreply.github.com>
2025-06-16 14:46:24 +0200

    Updated main.tsx with <React.StrictMode>

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import AuthProvider from './auth/AuthContext';
+import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-          <AuthProvider>
+          <React.StrictMode>
         <App />
-          </AuthProvider>
+          </React.StrictmMode>
 );
