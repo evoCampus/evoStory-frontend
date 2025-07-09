bd894b2bf466e1d7c0b1f0d16f30e9a443ea8c79
MilanGyulai <149778992+MilanGyulai@users.noreply.github.com>
2025-07-09 21:46:56 +0200

    User handling with backend (#8)
    * started to work on userHandling added AuthContext, LoginForm and LoginPage
    
    * created registration and login pages and methods to them (localstorage)
    
    * created new folder for authentication
    
    * made some changes
    
    * created api folder and changed necessary api calls in authentication
    
    * added style for buttons
    
    * formatted code
    
    * changed some style and text to hungarian
    
    * Deleted Settings page from App.tsx
    
    * Deleted Settings page from HomePage.tsx
    
    * Deleted Unnecessary ChapterPageBox from ChapterPage.tsx
    
    * Deleted comments which are not useful from ChapterPage.tsx
    
    * Deleted not used elements from index.css
    
    * Deleted extra lines from mock.ts
    
    * Changed Dashboard.tsx file because of Modal.tsx 
    
    Modal.tsx need to be added
    
    * Update HomePage.tsx buttons because they hadn't got the right classNames
    
    * deleted unneccessary characters
    
    * merged from main
    
    * added functions to clien.ts
    
    ---------
    
    Co-authored-by: Gyulai Milán <gyulai.milan04@gmail.com>

--- src/Client.ts
+++ src/Client.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { 
-    ChoiceApi, 
+import {
+    ChoiceApi,
     ChoiceDTO,
     CreateChoiceDTO,
     CreateSceneDTO,
     CreateStoryDTO,
-    SceneApi, 
+    CreateUserDTO,
+    LoginDTO,
+    SceneApi,
     SceneDTO,
     StoryApi,
-    StoryDTO
+    StoryDTO,
+    UserApi,
+    UserDTO
 } from "./api";
 
 
@@ -16,82 +20,106 @@ export default class Client {
     private readonly choiceAPI: ChoiceApi;
     private readonly sceneAPI: SceneApi;
     private readonly storyAPI: StoryApi;
+    private readonly userAPI: UserApi;
 
-    constructor ()
-    {
+
+    constructor() {
         const axiosInstance = axios.create({
             baseURL: "http://localhost:5006"
         });
         this.choiceAPI = new ChoiceApi(undefined, undefined, axiosInstance);
         this.sceneAPI = new SceneApi(undefined, undefined, axiosInstance);
         this.storyAPI = new StoryApi(undefined, undefined, axiosInstance);
+        this.userAPI = new UserApi(undefined, undefined, axiosInstance);
     }
 
-    async getChoices(): Promise<ChoiceDTO[]>{
+    async getChoices(): Promise<ChoiceDTO[]> {
         const { data } = await this.choiceAPI.getChoices();
 
         return data;
     }
 
-    async getScenes(): Promise<SceneDTO[]>{
+    async getScenes(): Promise<SceneDTO[]> {
         const { data } = await this.sceneAPI.getScenes();
 
         return data;
     }
 
-    async getStories(): Promise<StoryDTO[]>{
+    async getStories(): Promise<StoryDTO[]> {
         const { data } = await this.storyAPI.getStories();
 
         return data;
     }
 
-    async getChoiceById(id: string): Promise<ChoiceDTO>{
+    async getChoiceById(id: string): Promise<ChoiceDTO> {
         const { data } = await this.choiceAPI.getChoice(id);
 
         return data;
     }
 
-    async getSceneById(id: string): Promise<SceneDTO>{
+    async getSceneById(id: string): Promise<SceneDTO> {
         const { data } = await this.sceneAPI.getScene(id);
 
         return data;
     }
 
-    async getStoryById(id: string): Promise<StoryDTO>{
+    async getStoryById(id: string): Promise<StoryDTO> {
         const { data } = await this.storyAPI.getStory(id);
 
         return data;
     }
-    
-    async saveChoice(choice: CreateChoiceDTO): Promise<void>{
+
+    async saveChoice(choice: CreateChoiceDTO): Promise<void> {
         await this.choiceAPI.createChoice(choice);
     }
 
-    async saveScene(scene: CreateSceneDTO): Promise<CreateSceneDTO>{
+    async saveScene(scene: CreateSceneDTO): Promise<CreateSceneDTO> {
         const { data } = await this.sceneAPI.createScene(scene);
 
         return data;
     }
 
-    async saveStory(story: CreateStoryDTO): Promise<void>{
+    async saveStory(story: CreateStoryDTO): Promise<void> {
         await this.storyAPI.createStory(story);
     }
 
-    async deleteChoice(id: string): Promise<void>{
+    async deleteChoice(id: string): Promise<void> {
         await this.choiceAPI.deleteChoice(id);
     }
 
-    async deleteScene(id: string): Promise<void>{
+    async deleteScene(id: string): Promise<void> {
         await this.sceneAPI.deleteScene(id);
     }
 
-    async deleteStory(id: string): Promise<void>{
+    async deleteStory(id: string): Promise<void> {
         await this.storyAPI.deleteStory(id);
     }
 
-    async addStoryById(id: string): Promise<StoryDTO>{
+    async addStoryById(id: string): Promise<StoryDTO> {
         const { data } = await this.storyAPI.editStory(id);
 
         return data;
     }
+
+    async loginUser(user: LoginDTO): Promise<UserDTO> {
+        const { data } = await this.userAPI.login(user);
+
+        return data;
+    }
+    async registerUser(user: CreateUserDTO): Promise<void> {
+        await this.userAPI.createUser(user);
+    }
+
+    async getUser(userID: string): Promise<UserDTO> {
+        const { data } = await this.userAPI.getUser(userID);
+
+        return data;
+    }
+
+    async deleteUser(userID: string): Promise<UserDTO> {
+        const { data } = await this.userAPI.getUser(userID);
+
+        return data;
+    }
+
 }
